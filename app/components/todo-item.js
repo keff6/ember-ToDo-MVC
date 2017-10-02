3a6aebec532f1c4978031486493a06553f47cfba
Kevin Fallas <kev.fallas@gmail.com>
2017-10-02 14:42:11 -0600

    Editing and deleting functionality implemented

--- app/components/todo-item.js
+++ app/components/todo-item.js
@@ -1,24 +1,39 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-   actions:{
-     edit: function(){
-         console.log("jojosho");
-     }  
-   },
-    
-    isCompleted: function(key, value){
-            
-       var model = this.get('model')
-       
-       if(value === undefined){
-           return model.get('isCompleted');
-       }else{
-           model.set('isCompleted',value);
-           model.save();
-           return value;
-       }
-       
-   }.property('model.isCompleted')
-       
+    tagName: 'li',
+    classNameBindings: ['isEditing'],
+    isEditing: false,
+    actions: {
+        editTodo: function () {
+            this.toggleProperty('isEditing'); 
+        },
+        acceptChanges: function(){
+            this.set('isEditing',false);
+            if(Ember.isEmpty(this.get('todo.title'))){
+                this.send('removeTodo');
+            }else{
+                this.get('todo').save();
+            }
+        },
+        removeTodo: function(){
+            var todo = this.get('todo');
+            todo.deleteRecord();
+            todo.save();
+        }
+    },    
+    isCompleted: function (key, value) {
+
+        var model = this.get('todo');
+
+        if (value === undefined) {
+            return model.get('isCompleted');
+        } else {
+            model.set('isCompleted', value);
+            model.save();
+            return value;
+        }
+
+    }.property('todo.isCompleted')
+
 });
